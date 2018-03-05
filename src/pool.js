fe75ed40ccba5af991a7a041118b4448b3895acb
Davide Prati <lastexxit@gmail.com>
2018-03-05 20:08:36 +0100

    improve performance

--- src/pool.js
+++ src/pool.js
@@ -59,12 +59,7 @@ export default class Pool {
 
 	createObject(){
 		let palm = this.palmGeometry;
-		let mesh;
-		if(palm.attributes.isLeaf !== undefined){
-			mesh = new Mesh(palm, [this.materials[1], this.materials[0]]);
-		}else{
-			mesh = new Mesh(palm, this.materials[1]);
-		}
+		let mesh = new Mesh(palm, this.materials[1]);
 		mesh.rotateY(Math.PI / getRandom(-3, 3));
 
 		return mesh;
@@ -90,7 +85,7 @@ export default class Pool {
 					if (object_position + delay > horizon){
 						this.putObjectForwardTheCamera(camera_position_on_spline, i, flip_direction);
 					}
-				} else {
+				}else {
 					this.putObjectForwardTheCamera(camera_position_on_spline, i, flip_direction);
 				}
 			}
